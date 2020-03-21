0c19f31acaa7bb851c3b9cd1e8ce5b5368340b8e
adQuid <cricketfeir@gmail.com>
2020-03-21 12:54:39 -0400

    added to tests

--- src/test/game/TestGame.kt
+++ src/test/game/TestGame.kt
@@ -33,6 +33,8 @@ class TestGame {
         val game2 = Game(game.saveString())
         assertTrue(game == game2)
         assertTrue(game.players[0].acceptedDeals == game2.players[0].acceptedDeals)
+        assertTrue(game.titles.size == game2.titles.size)
+        assertTrue(game.titles.toList()[0] == game2.titles.toList()[0])
         game.players.forEach { player ->
             assertTrue { player.name == game2.characterById(player.id).name };
             assertTrue { player.titles.size == game2.characterById(player.id).titles.size }
