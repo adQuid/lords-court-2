8e32076699f3ffe48227d54bac7ee52117d4e681
adQuid <cricketfeir@gmail.com>
2020-05-06 17:58:39 -0400

    zoomy map fun

--- src/ui/specialdisplayables/worldgen/WorldEditorMainMenu.kt
+++ src/ui/specialdisplayables/worldgen/WorldEditorMainMenu.kt
@@ -1,19 +1,32 @@
 package ui.specialdisplayables.worldgen
 
 import javafx.event.EventHandler
+import javafx.geometry.Rectangle2D
 import javafx.scene.Scene
 import javafx.scene.layout.GridPane
-import shortstate.ShortStateCharacter
+import main.UIGlobals
 import ui.NoPerspectiveDisplayable
-import ui.PerspectiveDisplayable
 import ui.componentfactory.UtilityComponentFactory
+import kotlin.math.max
+import kotlin.math.min
 
 class WorldEditorMainMenu: NoPerspectiveDisplayable() {
+
+    val baseWidth = UIGlobals.totalWidth()
+    val baseHeight = UIGlobals.totalHeight()
+
+    val imageView = UtilityComponentFactory.imageView("maps/testland/background.png")
+
+    var focusX = 1000.0
+    var focusY = 750.0
+    var zoom = 0.5
 
     override fun display(): Scene {
         val pane = GridPane()
 
-        pane.add(UtilityComponentFactory.imageView("maps/testland/background.png"), 0, 0)
+        setViewPort()
+        imageView.onScroll = EventHandler { event -> changeViewport(event.x - baseWidth/2.0,event.y - (baseHeight*0.8)/2.0, 0.01 * event.deltaY) }
+        pane.add(imageView, 0, 0)
 
         val bottomPane = GridPane()
         bottomPane.add(UtilityComponentFactory.proportionalBackButton(2.0), 0, 0)
@@ -22,5 +35,41 @@ class WorldEditorMainMenu: NoPerspectiveDisplayable() {
         pane.add(bottomPane, 0, 11)
 
         return Scene(pane)
+    }
+
+    private fun changeViewport(x: Double, y: Double, scaleDelta: Double){
+        val zoomDelta = max(min(scaleDelta, 1.0), -1.0)
+        if(zoomDelta > 0){
+            focusX += x/(3*zoom)
+            focusY += y/(3*zoom)
+        }
+        zoom += zoomDelta
+        setViewPort()
+    }
+
+    private fun setViewPort(){
+        if(zoom < 0 || (baseWidth / zoom > imageView.image.width || baseHeight / zoom > imageView.image.height)){
+            zoom = min(baseWidth / imageView.image.width, baseHeight / imageView.image.height)
+        }
+        if(zoom > 10.0){
+            zoom = 10.0
+        }
+        val displayWidth = baseWidth/zoom
+        val displayHeight = baseHeight/zoom
+
+        if(focusX - (displayWidth/2) < 0){
+            focusX = displayWidth / 2
+        }
+        if(focusX + (displayWidth/2) > imageView.image.width){
+            focusX = imageView.image.width- (displayWidth / 2)
+        }
+        if(focusY - (displayHeight/2) < 0){
+            focusY = displayHeight / 2
+        }
+        if(focusY + (displayHeight/2) > imageView.image.height){
+            focusY = imageView.image.height- (displayHeight / 2)
+        }
+
+        imageView.viewport = Rectangle2D(focusX - (displayWidth/2.0),focusY - (displayHeight/2.0), displayWidth, displayHeight)
     }
 }
